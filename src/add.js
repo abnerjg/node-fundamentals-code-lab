74fa75812a0c4a222250532a0a41fc30dba1ce4c
Abner Jimenez <ajjg21@gmail.com>
2023-09-08 13:52:33 -0600

    finalizado

--- src/add.js
+++ src/add.js
@@ -1,1 +1,14 @@
 import chalk from "chalk";
+
+export const add = (_numero1, _numero2) => _numero1 + _numero2;
+
+export const showResult = (_numero1, _numero2) => {
+    
+    const result = add (_numero1, _numero2);
+
+    if (result>=5){
+        return chalk.green(result);
+    } else {
+        return chalk.red(result);
+    } 
+}
