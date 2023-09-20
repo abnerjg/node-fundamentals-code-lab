c19b65236d9a8d959eb48a93abf3cc596b1611fe
Abner Jimenez <ajjg21@gmail.com>
2023-09-20 13:47:50 -0600

    finalizado

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
-function getRandomNumber() {}
+// Definir la función getRandomNumber que retorna una promesa
+function getRandomNumber() {
+  return new Promise((resolve, reject) => {
+    // Usar setTimeout para simular un proceso largo de 3 segundos
+    setTimeout(() => {
+      // Generar un número aleatorio entre 0 y 10 usando Math.random()
+      const randomNumber = Math.floor(Math.random() * 10); // Números entre 0 y 10
+      // Resolver la promesa con el número aleatorio generado
+      resolve(randomNumber);
+    }, 3000); // Retraso de 3 segundos (3000 milisegundos)
+  });
+}
 
 module.exports = getRandomNumber;
